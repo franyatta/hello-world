7467091aa7c65a64b7c17d64b0726dbc2d427061
franyatta <95552724+franyatta@users.noreply.github.com>
2023-09-06 16:15:58 -0400

    Add files via upload

---
+++ Francine_Vo_Lab1.java
@@ -0,0 +1,9 @@
+public class Francine_Vo_Lab1 {
+	public static void main(String[] args) {
+		//source code for francine vo lab 1
+		System.out.println("Student Name: Francine Vo");
+		System.out.println("Student UIN: ENTER ID HERE");
+		System.out.println("Date: 08/30/2023");
+	}
+
+}
